34d2bb19a07b4c7c1e9fc37a726ccca8146f9b4d
Karanveer Singh <karan.cse.dev@gmail.com>
2026-07-11 16:09:36 +0530

    Organized all cpp files into folders

---
+++ week1/user input/user.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+    //variable decalre
+    bool marks;
+    std::cout << "Enter your marks" << endl;
+
+    //input le rhe h yha pr
+    cin >> marks;
+    cout << "Babbar's marks: " << marks << endl;
+
+    return 0;
+}
---
+++ week1/variables and datatypes/variables.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    //variable define
+    // int age = 25;
+    // cout << "Babbar's Age is: " << age << endl;
+
+    //decalartion
+    //int age;
+    //cout << age;
+
+    //definition
+    // int age = 21;
+    // cout << age << endl;
+
+    // //manipulation or updation
+    // age = 101;
+    // cout << age << endl;
+
+
+    //int
+    // int count = 5;
+    // //float
+    // float share = 3.14;
+    // //char
+    // char alphabet = 'z';
+    // //double 
+    // double weight = 55.69887;
+    // //boolean
+    // bool isMale = true;
+    // // bool isChild = 1;
+    // // bool isFemale = false;
+    // // bool isGirl = 0;
+
+    // cout << count << endl;
+    // cout << share << endl;
+    // cout << alphabet << endl;
+    // cout << weight << endl;
+    // cout << isMale << endl;
+
+    //int age = 12;
+    //cout << sizeof(age) << endl;
+    int marks = 5;
+    cout << marks << endl;
+
+    return 0;
+}
