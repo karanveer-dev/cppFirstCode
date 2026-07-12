1b63f1bf41c92724f7dfb25915093d9df92c4c84
Karanveer Singh <karan.cse.dev@gmail.com>
2026-07-12 06:43:23 +0530

    ternary operator code completed

---
+++ week1/control flow/ternary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// int age = 18;
+// (age >= 18) ? cout <<"Can vote" : cout <<"cannot vote" ;
+
+int x = 20;
+int y = 40;
+
+int result = (x>y) ? x : y;
+
+cout << "Result:" << result << endl; 
+
+return 0;
+}
