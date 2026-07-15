ed193578473a2377c577e81039e072e1ef2df788
Karanveer Singh <karan.cse.dev@gmail.com>
2026-07-15 15:58:35 +0530

    Loops and break statement practice

---
+++ week1/loops/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //counting from 1 to 100 
+// for(int i =1; i<=100; i=i+1) {
+//     cout<< i <<endl;
+// }
+
+//counting from 51 to 89
+// for(int i = 51; i<=89 ; i=i+1){
+//     cout<<i<<endl ;
+// }
+
+// for(int i = 1; i<=10; i=i+1) {
+//    cout<< i <<" ";
+//    if(i==5){
+//     break;
+//    }
+// }
+
+
+
+for(int i = 1 ; i<=7;i=i+1){
+    if(i ==3){
+    continue ;
+    }
+    cout<<i<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
